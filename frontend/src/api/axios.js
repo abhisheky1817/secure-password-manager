59779d9eb88324b7539b25f0b10cb728955f171a
abhisheky1817 <abhisheky1817@gmail.com>
2025-12-14 01:16:54 +0530

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,22 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://secure-password-manager-z97r.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// Attach token automatically
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
